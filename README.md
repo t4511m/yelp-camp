31427b21d571a1625223e29e0ef4022cc776ee4c
Taslim <taslim.edu@gmail.com>
2024-02-19 07:45:15 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Run mongodb
+
+```
+docker run -p 8080:27017 mongo
+```
---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  db: {
+    url: "mongodb://localhost:8080/yelp-camp",
+  },
+  app: {
+    port: 3000,
+  },
+};
+
+module.exports = config;
